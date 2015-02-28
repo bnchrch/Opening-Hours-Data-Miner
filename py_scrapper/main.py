aae746d1cb30f0907d72019bb86702a9d5296ac8
Brett Binnersley <brettbinnersley14@gmail.com>
2015-02-28 04:34:17 -0800

    define a list of API keys and it will spawn a thread for each one

--- py_scrapper/main.py
+++ py_scrapper/main.py
@@ -5,28 +5,31 @@ from fetch import fetch_remote, fetch_remote_json #For fetching data from url
 
 searchNearbyCmd = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 detailsCmd =      'https://maps.googleapis.com/maps/api/place/details/json?'
-api_key = 'AIzaSyBB9KLa1_lrVbPGagttplCeVtoWZ5f0d0o'
 req_tally = 0;
 has_hours = 0;
 
+#List of all api keys. We should all get one if possible
+apiKeys = ['AIzaSyBB9KLa1_lrVbPGagttplCeVtoWZ5f0d0o']
+
 class request_details(threading.Thread):
-  def __init__ (self, place_id):
+  def __init__ (self, apiKey, place_id):
     threading.Thread.__init__(self)
     self.place_id = place_id
+    self.apiKey = apiKey
 
   def run(self):
-    params = {'key' : api_key, 'placeid' : self.place_id}
+    params = {'key' : self.apiKey, 'placeid' : self.place_id}
     json = fetch_remote_json(detailsCmd, params);
     response = json[1]
-    print "PID: " + str(self.place_id)
-    print response #Do something with this here
+    print response #Do something with placeId and response here
 
 class find_places(threading.Thread):
-  def __init__ (self, req_url, params, timeout):
+  def __init__ (self, req_url, apiKey, params, timeout):
     threading.Thread.__init__(self)
     self.req_url = req_url
     self.params = params
     self.timeout = timeout
+    self.apiKey = apiKey
 
   def run(self):
     global has_hours, req_tally
@@ -39,16 +42,17 @@ class find_places(threading.Thread):
     token = response.get('next_page_token', 0)
     if token:
       print "Got One: " + token[:64] + "..."
-      find_places(searchNearbyCmd, {'pagetoken' : token, 'key' : api_key}, 3).start()
+      find_places(searchNearbyCmd, self.apiKey, {'pagetoken' : token, 'key' : self.apiKey}, 3).start()
 
     for res in results:
       if res.get('opening_hours', 0):
         has_hours += 1
-        request_details(res['place_id']).start()
+        request_details(self.apiKey, res['place_id']).start()
 
     req_tally += len(results)
     print "Req|HasTally: " + str(req_tally) + " | " + str(has_hours)
 
 
-#Run code here
-find_places(searchNearbyCmd, {'key' : api_key,'radius' : '100','location' : '48.4222,-123.3657'}, 0).start()
+#Spawn a thread for each api key
+for key in apiKeys:
+  find_places(searchNearbyCmd, key, {'key' : key, 'radius' : '100' , 'location' : '48.4222,-123.3657'}, 0).start()
